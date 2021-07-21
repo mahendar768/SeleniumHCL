2639d0115a0acb7ba301eab8f802957c6518cd12
unknown <mahender@fininfocom.com>
2021-07-21 19:06:47 +0530

    added app demo project

---
+++ eposhybrid/app-demo/src/main/java/com/app/pages/Applogin.java
@@ -0,0 +1,52 @@
+package com.app.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+
+import com.app.base.AppTestbase;
+
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+
+public class Applogin extends AppTestbase{
+
+	
+	@FindBys({@FindBy(xpath ="/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[1]/android.view.View[2]/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.widget.EditText")})
+	AndroidElement textbox;
+		 
+	@AndroidFindBy(xpath ="/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[1]/android.view.View[2]/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[4]")
+	AndroidElement searchbutton ;
+
+	public  void Loginpagethis()
+	{
+		PageFactory.initElements(driver, this);
+		
+	}
+	
+	public AndroidDriver<MobileElement> postcode(String code)
+	{
+		
+		System.out.println("driver5=="+driver);
+		textbox.sendKeys(code);
+		searchbutton.click();
+		//driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[1]/android.view.View[2]/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.widget.EditText")).sendKeys("EH14 2JT");
+		//driver.findElement(By.xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[1]/android.view.View[2]/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[4]")).click();
+		
+		 return driver;
+			
+			
+			
+	}
+	
+	
+	
+	
+}
