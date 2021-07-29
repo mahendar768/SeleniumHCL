02615f793cc9c34168c76a83cbc85d54e7eeab90
unknown <mahender@fininfocom.com>
2021-07-29 21:03:57 +0530

    commited new firstproject

--- Hdfc/src/test/java/newproject/Firstproject.java
+++ Hdfc/src/test/java/newproject/Firstproject.java
@@ -8,7 +8,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Firstprojectnew {
+public class Firstproject {
 	public static WebDriver driver;
 
 	@BeforeClass
@@ -42,7 +42,7 @@ public class Firstprojectnew {
 	}
 
 	@Test
-	public static void doubleclick ()
+	public static void doubleclickmasteroii ()
 	{
 		
 	}
