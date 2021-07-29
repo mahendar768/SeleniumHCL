3b51878d6526c07e5a920fedf2196feda0329ea1
unknown <mahender@fininfocom.com>
2021-07-29 20:45:40 +0530

    firstproject renamed to  firstproject new

--- Hdfc/src/test/java/newproject/Firstprojectnew.java
+++ Hdfc/src/test/java/newproject/Firstprojectnew.java
@@ -8,7 +8,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Firstproject {
+public class Firstprojectnew {
 	public static WebDriver driver;
 
 	@BeforeClass
