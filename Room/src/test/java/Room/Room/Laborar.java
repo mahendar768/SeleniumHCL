c75913ca87f369b1abddf1e3d1421483b1022636
unknown <mahender@fininfocom.com>
2021-08-02 19:55:25 +0530

    room project adding to git

---
+++ Room/src/test/java/Room/Room/Laborar.java
@@ -0,0 +1,11 @@
+package Room.Room;
+
+public class Laborar {
+
+	public static void main(String[] args) {
+		int hd = 20;
+		System.out.println(hd);
+
+	}
+
+}
---
+++ Room/src/test/java/Room/Room/Laborardelets.java
@@ -0,0 +1,10 @@
+package Room.Room;
+
+public class Laborardelets {
+
+	public static void main(String[] args) {
+		int hdj = 20;
+		System.out.println(hdj);
+	}
+
+}
