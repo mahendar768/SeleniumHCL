21ba5baf7494ab609308fe9467aa2c06ebc46321
welcome <mahender@fininfocom.com>
2021-08-27 19:12:13 +0530

    new class added

--- Room/src/test/java/Room/Room/Laborar.java
+++ Room/src/test/java/Room/Room/Laborar.java
@@ -1,10 +1,17 @@
 package Room.Room;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class Laborar {
 
-	public static void main(String[] args) {
-		int hd = 200;
-		System.out.println(hd);
+	public static WebDriver driver;
+	@Test
+	public void inti()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\welcome\\eclipse-workspace\\Room\\Drivers\\chromedriver.exe");
+	driver = new ChromeDriver();
 
 	}
 
