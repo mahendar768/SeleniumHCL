7716ef21265a412af3009f31b605e5121e0b4b64
unknown <mahender@fininfocom.com>
2022-11-20 15:18:07 +0530

    Revert "change gitURL to mahendar768 on 20-11-2022"
    This reverts commit 108174376a163d14be0cd9900d7619e18704c7b1.

--- jking/src/test/java/jking/jking/Trail.java
+++ jking/src/test/java/jking/jking/Trail.java
@@ -15,8 +15,6 @@ public class Trail {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\welcome\\eclipse-workspace\\jking\\drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();	
 		driver.get("https://www.redbus.in/");
-		driver.getTitle();
-		
 	}
 
 }
