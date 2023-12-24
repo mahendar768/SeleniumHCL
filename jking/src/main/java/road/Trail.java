9b17b935c9783a8da4d74c8f39cb68288a77cab7
mahendar768 <mahendar768@gmail.com>
2023-12-24 10:18:05 +0530

    changed url just for testing

--- jking/src/main/java/road/Trail.java
+++ jking/src/main/java/road/Trail.java
@@ -14,7 +14,7 @@ public class Trail {
 	{
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\welcome\\eclipse-workspace\\jking\\drivers\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();	
-		driver.get("https://www.redbus.in/");
+		driver.get("https://www.redbus.in");
 	}
 
 }
